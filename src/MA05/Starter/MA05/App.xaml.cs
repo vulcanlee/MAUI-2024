8e8aa31962bd0c7a62f62064d81c8768f01255fa
Vulcan <vulcan.lee@gmail.com>
2023-12-08 16:47:13 +0800

    MA05|使用 Code-Behind 方式進行 MAUI 專案開發

---
+++ src/MA05/Starter/MA05/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MA05
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/MA05/Starter/MA05/HomePage.xaml.cs
@@ -0,0 +1,10 @@
+namespace MA05;
+
+public partial class HomePage : ContentPage
+{
+    // 這裡是 Code-Behind 的程式碼
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
