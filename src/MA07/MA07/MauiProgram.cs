233814b1771c2a7c39e792c7bd2ea94fa959f1a1
Vulcan <vulcan.lee@gmail.com>
2024-01-15 11:25:35 +0800

    .NET 8 MAUI 使用服務定位器 Service Locator 手動注入相依物件

--- src/MA07/MA07/MauiProgram.cs
+++ src/MA07/MA07/MauiProgram.cs
@@ -1,35 +1,33 @@
-﻿using MA07.Helpers;
-using MA07.Services;
+﻿using MA07.Services;
 using MA07.ViewModels;
 using MA07.Views;
 using Microsoft.Extensions.Logging;
 
-namespace MA07
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+namespace MA07;
 
-            builder.Services.AddTransient<ServiceLocatorPageViewModel>();
-            builder.Services.AddTransient<ServiceLocatorPage>();
-            builder.Services.AddTransient<SendEmailService>();
-            builder.Services.AddTransient<SendSmsService>();
-            builder.Services.AddTransient<SendLineService>();
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddTransient<ServiceLocatorPageViewModel>();
+        builder.Services.AddTransient<ServiceLocatorPage>();
+        builder.Services.AddTransient<SendEmailService>();
+        builder.Services.AddTransient<SendSmsService>();
+        builder.Services.AddTransient<SendLineService>();
 
 #if DEBUG
-            builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
