a05b1b74f1b3bfb96159e2cddeed387d58826ade
Vulcan <vulcan.lee@gmail.com>
2024-01-23 17:00:12 +0800

    .NET 8 MAUI 使用相依性注入設計模式進行 MVVM App 開發

--- src/MA10/MA10/MainPage.xaml.cs
+++ src/MA10/MA10/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MA10;
+﻿using System.Linq;
+namespace MA10;
 
 public partial class MainPage : ContentPage
 {
@@ -8,5 +9,17 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    private void ShowBindingContextButton_Clicked(object sender, EventArgs e)
+    {
+        Console.WriteLine($"ThisPage BindingContext: {ThisPage.BindingContext}");
+        Console.WriteLine($"ThisVerticalStackLayout BindingContext: {ThisVerticalStackLayout.BindingContext}");
+        Console.WriteLine($"ThisStackLayout BindingContext: {ThisStackLayout.BindingContext}");
+        Console.WriteLine($"ThisStackLayoutLabel BindingContext: {ThisStackLayoutLabel.BindingContext}");
+        Console.WriteLine($"ThisCollectionView BindingContext: {ThisCollectionView.BindingContext}");
+
+        StackLayout stackLayout = ThisCollectionView.GetVisualTreeDescendants()[1] as StackLayout;
+        Console.WriteLine($"ThisCollectionViewItem BindingContext: {stackLayout.BindingContext}");
     }
 }
