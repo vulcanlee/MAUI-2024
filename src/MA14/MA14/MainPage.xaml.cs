50e400c03d9d61d59238bfa6ff11bd8ed1e510db
Vulcan <vulcan.lee@gmail.com>
2024-02-15 16:43:43 +0800

    .NET 8 MAUI 了解 INotifyPropertyChanged 實作與運作行為

--- src/MA14/MA14/MainPage.xaml.cs
+++ src/MA14/MA14/MainPage.xaml.cs
@@ -1,58 +1,56 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
 
-namespace MA14
+namespace MA14;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+    MainPageViewModel mainPageViewModel = new MainPageViewModel();
+    public MainPage()
     {
-        int count = 0;
-        MainPageViewModel mainPageViewModel = new MainPageViewModel();
-        public MainPage()
+        InitializeComponent();
+        BindingContext = mainPageViewModel;
+
+        UserNameEntry.PropertyChanged += UserNameEntry_PropertyChanged;
+        AgeEntry.PropertyChanged += (s, e) =>
         {
-            InitializeComponent();
-            BindingContext = mainPageViewModel;
-
-            UserNameEntry.PropertyChanged += UserNameEntry_PropertyChanged;
-            AgeEntry.PropertyChanged += (s, e) =>
-            {
-                Debug.WriteLine($"[View] Age Entry 的 Text 屬性已經有變動 (PropertyChanged) : {e.PropertyName}");
-            };
-            //UserNameEntry.PropertyChanging += UserNameEntry_PropertyChanging;
-            mainPageViewModel.PropertyChanged += (s, e) =>
-            {
-                Debug.WriteLine($"訂閱 [ViewModel] 的 PropertyChanged 事件被觸發 : {e.PropertyName}");
-            };
-        }
-
-        private void UserNameEntry_PropertyChanging(object sender, Microsoft.Maui.Controls.PropertyChangingEventArgs e)
+            Debug.WriteLine($"[View] Age Entry 的 Text 屬性已經有變動 (PropertyChanged) : {e.PropertyName}");
+        };
+        //UserNameEntry.PropertyChanging += UserNameEntry_PropertyChanging;
+        mainPageViewModel.PropertyChanged += (s, e) =>
         {
-            #region 消除除厝噪音
-            if (e.PropertyName == "IsFocused" || e.PropertyName == "CursorPosition")
-            {
-                return;
-            }
-            #endregion
-
-            Debug.WriteLine($"[View] UserName Entry 的 Text 屬性即將有變動 (PropertyChanging) : {e.PropertyName}");
-        }
-
-        private void UserNameEntry_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            #region 消除除厝噪音
-            if (e.PropertyName == "IsFocused" || e.PropertyName == "CursorPosition"
-                || e.PropertyName == "SelectionLength")
-            {
-                return;
-            }
-            #endregion
-
-            Debug.WriteLine($"[View] UserName Entry 的 Text 屬性已經有變動 (PropertyChanged) : {e.PropertyName}");
-        }
-
-        private void SayHiBtnClicked(object sender, EventArgs e)
-        {
-            mainPageViewModel.UserName = DateTime.Now.ToString();
-        }
+            Debug.WriteLine($"訂閱 [ViewModel] 的 PropertyChanged 事件被觸發 : {e.PropertyName}");
+        };
     }
 
+    private void UserNameEntry_PropertyChanging(object sender, Microsoft.Maui.Controls.PropertyChangingEventArgs e)
+    {
+        #region 消除除厝噪音
+        if (e.PropertyName == "IsFocused" || e.PropertyName == "CursorPosition")
+        {
+            return;
+        }
+        #endregion
+
+        Debug.WriteLine($"[View] UserName Entry 的 Text 屬性即將有變動 (PropertyChanging) : {e.PropertyName}");
+    }
+
+    private void UserNameEntry_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        #region 消除除厝噪音
+        if (e.PropertyName == "IsFocused" || e.PropertyName == "CursorPosition"
+            || e.PropertyName == "SelectionLength")
+        {
+            return;
+        }
+        #endregion
+
+        Debug.WriteLine($"[View] UserName Entry 的 Text 屬性已經有變動 (PropertyChanged) : {e.PropertyName}");
+    }
+
+    private void SayHiBtnClicked(object sender, EventArgs e)
+    {
+        mainPageViewModel.UserName = DateTime.Now.ToString();
+    }
 }
