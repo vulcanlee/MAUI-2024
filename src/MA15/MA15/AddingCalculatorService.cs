74163965b619ccb0ab23028759ca4be150314412
Vulcan <vulcan.lee@gmail.com>
2024-02-17 09:51:41 +0800

    .NET 8 MAUI 使用 Blazor Hybrid 架構設計計算機功能

--- src/MA15/MA15/AddingCalculatorService.cs
+++ src/MA15/MA15/AddingCalculatorService.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MA15;
 
-namespace MA15
+public class AddingCalculatorService
 {
-    public class AddingCalculatorService
+    public async Task<string> AddAsync(int a, int b)
     {
-        public async Task<string> AddAsync(int a, int b)
-        {
-            int estimate = 5000;
-            string url = $"https://businessblazor.azurewebsites.net/api/RemoteService/" +
-                $"Add/{a}/{b}/{estimate}";
+        int estimate = 5000;
+        string url = $"https://businessblazor.azurewebsites.net/api/RemoteService/" +
+            $"Add/{a}/{b}/{estimate}";
 
-            HttpClient client = new HttpClient();
+        HttpClient client = new HttpClient();
 
-            string result = await client.GetStringAsync(url);
-            return result;
-        }
+        string result = await client.GetStringAsync(url);
+        return result;
     }
 }
